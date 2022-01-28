702d3c02a28f43cb35d59b36d857768d03169f08
Guillaume V <guillaume.v.gv@gmail.com>
2022-01-28 13:41:08 +0100

    corrections

--- joueur.h
+++ joueur.h
@@ -2,34 +2,13 @@
 #include <vector>
 
 enum class Dir{GAUCHE,DROITE,HAUT,BAS};
-enum class Potion{VIE}
-enum class Arme{EPEE, ARC}
+enum class Potion{VIE};
+enum class Arme{EPEE, ARC};
 
-class Joueur public Personnage{
-    Joueur(std::vector<int> pos);
-
-
-    protected:
-    int piecesOr = 0;
-    std::vector<Potion> potions;
-    std::vector<Arme> armes;
-
-};
-
-
-class Personnage public Entite{
-    public:
-    Personnage(std::vector<int> pos);
-    void move(Dir dir);
-
-    protected:
-    int PV;
-
-}
 
 class Entite{
-    Entite(std::vector<int> pos);
     public:
+    Entite(std::vector<int> pos);
     std::vector<int> get_pos()const;
     void set_pos(std::vector<int> pos);
         
@@ -37,4 +16,29 @@ class Entite{
     std::vector<int> pos;
 
 
-}
+};
+
+class Personnage : public Entite{
+    public:
+    Personnage(std::vector<int> pos);
+    void combattre(Personnage adversaire);
+    void move(Dir dir);
+
+    protected:
+    int PV;
+    int force;
+
+};
+
+class Joueur : public Personnage{
+    Joueur(std::vector<int> pos);
+
+
+
+    protected:
+    int piecesOr;
+    std::vector<Potion> potions;
+    std::vector<Arme> armes;
+    Arme armePrincipale;
+
+};

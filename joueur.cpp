f66ced175458d7648d1e6c965ac250500a106608
Guillaume V <guillaume.v.gv@gmail.com>
2022-01-28 14:55:43 +0100

    aaa

--- joueur.cpp
+++ joueur.cpp
@@ -23,7 +23,7 @@ void Personnage::move(Dir dir){
             this->_pos = std::vector<int>(x+1,y);
             break;
         case Dir::HAUT:
-            this->_pos = std::vector<int>(x,y-1));
+            this->_pos = std::vector<int>(x,y-1);
             break;
         case Dir::BAS:
             this->set_pos(std::vector<int>(x,y+1));

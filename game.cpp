3ad660309b324e81d6ca9f0825bc9d4acabd4214
clementboutaric2 <93884878+clementboutaric2@users.noreply.github.com>
2022-01-28 10:10:18 +0100

    mise en place boucle de jeu embryonnaire

--- game.cpp
+++ game.cpp
@@ -11,6 +11,28 @@ class game
 
     public:
 
-    game();
+    game(const std::string&);
     void lancer();
+    void background_clear();
+    void afficher();
 };
+
+game::game(const std::string& Path, const std::vector<int>& Position) : terrain(Path), joueur(Position) {}
+
+void game::background_clear(){
+  // efface la sortie
+  int out = system("clear");
+  if (out != 0){
+    std::cerr << "clear command failed" << std::endl;
+    exit(3);
+  }
+}
+
+void game::afficher(){
+    this->terrain->affiche();
+    this->joueur->affiche();
+}
+
+void lancer(){
+    while(true){
+        if (key_event()

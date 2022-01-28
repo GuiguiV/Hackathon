2451431284788640b4cc30240729b67aeb98514d
Guillaume V <guillaume.v.gv@gmail.com>
2022-01-28 11:45:39 +0100

    classe personnage

--- joueur.h
+++ joueur.h
@@ -3,19 +3,32 @@
 
 enum class Dir{GAUCHE,DROITE,HAUT,BAS};
 
-class Joueur{
-
-    Joueur(std::vector<int>);
-     std::vector<int> get_pos()const;
-    void set_pos(std::vector<int> pos);
-    void move(Dir dir);
+class Joueur public Personnage{
+    Joueur(std::vector<int> pos);
 
 
-
+    protected:
     int piecesOr = 0;
     //ajouter des enum pour les trucs suivants : 
     int nbPotionsHeal = 0;
     int typeArme = 1; //1 : epee, 2:arc
-    std::vector<int> pos;
 
 };
+
+
+class Personnage{
+    public:
+    Personnage(std::vector<int>);
+     std::vector<int> get_pos()const;
+    void set_pos(std::vector<int> pos);
+    void move(Dir dir);
+
+    protected:
+    std::vector<int> pos;
+    int PV;
+
+}
+
+class Mechant public Personnage{
+
+}
